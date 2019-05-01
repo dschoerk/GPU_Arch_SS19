acc18596513af093e07d253e26d7c827a40173b8
dschoerk <no@mail.com>
2019-05-01 17:58:00 +0200

    had to butcher the error macros as error.h doesnt exist on windows

--- utils.h
+++ utils.h
@@ -3,48 +3,67 @@
 
 #include <cstdlib>
 #include <cstdarg>
-#include <getopt.h>
+
+/// - can be removed?
+#ifdef __linux__ 
+    #include <getopt.h>
+#endif
+///
+
 #include <cerrno>
-#include <error.h>
+    
 
-#define ERROR(format, ...)			\
-    error_at_line(				\
-        EXIT_SUCCESS,				\
-	errno,					\
-	__FILE__,				\
-	__LINE__,				\
-	format,					\
-        ## __VA_ARGS__				\
-	)
+#ifdef __linux__ 
+    #include <error.h>
 
-#define ERROR_EXIT(format, ...)			\
-    error_at_line(				\
-        EXIT_FAILURE,				\
-	errno,					\
-	__FILE__,				\
-	__LINE__,				\
-	format,					\
-        ## __VA_ARGS__				\
-	)
-
-#ifdef DEBUG
-#define TRACE(format, ...)			\
-    trace(					\
-        "%s [%s, %s(), line %d]: " format,	\
-        program_invocation_name,		\
+    #define ERROR(format, ...)			\
+        error_at_line(				\
+            EXIT_SUCCESS,				\
+        errno,					\
         __FILE__,				\
-        __func__,				\
         __LINE__,				\
-        ## __VA_ARGS__				\
+        format,					\
+            ## __VA_ARGS__				\
         )
-extern bool verbose;
 
-extern void trace(
-    const char *fmt,
-    ...
-    );
+    #define ERROR_EXIT(format, ...)			\
+        error_at_line(				\
+            EXIT_FAILURE,				\
+        errno,					\
+        __FILE__,				\
+        __LINE__,				\
+        format,					\
+            ## __VA_ARGS__				\
+        )
+
+    #ifdef DEBUG
+        #define TRACE(format, ...)			\
+            trace(					\
+                "%s [%s, %s(), line %d]: " format,	\
+                program_invocation_name,		\
+                __FILE__,				\
+                __func__,				\
+                __LINE__,				\
+                ## __VA_ARGS__				\
+                )
+        extern bool verbose;
+
+        extern void trace(
+            const char *fmt,
+            ...
+            );
+    #else
+        #define TRACE(format, ...)
+    #endif
 #else
-#define TRACE(format, ...)
-#endif // DEBUG
+    #define ERROR(format, ...) /*\
+        printf(				\
+        format,					\
+            ## __VA_ARGS__				\
+        ); exit(1)*/
 
+
+    #define ERROR_EXIT(format, ...)
+    #define TRACE(format, ...)
+#endif
 #endif
