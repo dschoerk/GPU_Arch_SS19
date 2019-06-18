09256c8d363eec9f71e6ef2ffb8f701e7b2911fb
dschoerk <no@mail.com>
2019-06-19 00:22:37 +0200

    better thrust parallelization
    still a little faulty

--- streaming_min_max_thrust_cuda.cu
+++ streaming_min_max_thrust_cuda.cu
@@ -46,10 +46,75 @@ struct ShiftedWindowMinMaxStep
             // __syncthreads();
         }
 
-        out_min[idx] = min;
+        out_min[idx] = min;        
         out_max[idx] = max;
 
         //out_min[idx] = idx;
+    }
+};
+
+struct ShiftedWindowMinMaxStep2
+{
+    int win_size;
+    int data_size;
+    thrust::device_ptr<float> data;
+    thrust::device_ptr<float> out_max;
+    thrust::device_ptr<float> out_min;
+    thrust::device_ptr<float> tmp_max;
+    thrust::device_ptr<float> tmp_min;
+
+    int win_size_log2; // next smaller power of 2
+    
+    ShiftedWindowMinMaxStep2(
+        thrust::device_ptr<float> _data, 
+        int _win_size, 
+        int _data_size,
+        thrust::device_ptr<float> _tmp_min, 
+        thrust::device_ptr<float> _tmp_max,
+        thrust::device_ptr<float> _out_min, 
+        thrust::device_ptr<float> _out_max
+    ) : data(_data), win_size(_win_size), out_min(_out_min), out_max(_out_max), tmp_min(_tmp_min), tmp_max(_tmp_max) , data_size(_data_size)
+    {
+        for(win_size_log2 = 0; 1 << win_size_log2 <= win_size; ++win_size_log2); // find next smaller power of 2 to win_size
+        win_size_log2--;
+
+        printf("win_size_log2: %d\n", win_size_log2);
+        printf("summation steps: %d\n", win_size - (1 << win_size_log2));
+    }
+
+    __device__ void operator() (const int &k)
+    {
+        //if (k > 250)
+        /*{
+            printf("v: %d %f\n", k, data[k]);
+        }*/
+        
+        for(int d = 0; d < win_size_log2; ++d) // naive and incomplete prefix sum
+        {
+            if(k + (1 << d) < data_size)
+            {
+                tmp_min[k] = fminf(tmp_min[k], tmp_min[k + (1 << d)]);
+                tmp_max[k] = fmaxf(tmp_max[k], tmp_max[k + (1 << d)]);
+            }
+
+            __syncthreads();
+        }
+
+        if(k < data_size - win_size + 1) 
+        {
+            out_min[k] = tmp_min[k];
+            out_max[k] = tmp_max[k];
+
+            __syncthreads();
+
+            for(int i = 0; i < win_size - (1 << win_size_log2); ++i) // sum up the rest
+            {
+                out_min[k] = fminf(out_min[k], tmp_min[k+i+1]);
+                out_max[k] = fmaxf(out_max[k], tmp_max[k+i+1]);
+
+                __syncthreads();
+            }
+        }
     }
 };
 
@@ -64,8 +129,11 @@ void streaming_min_max_thrust_calc(
 
     // transfer data to the device
     thrust::device_vector<float> d_vec(array); // = h_vec;
-    thrust::device_vector<float> d_minima(d_vec.size() - width + 1);
-    thrust::device_vector<float> d_maxima(d_vec.size() - width + 1);
+    thrust::device_vector<float> d_minima(d_vec.size());
+    thrust::device_vector<float> d_maxima(d_vec.size());
+
+    thrust::device_vector<float> d_tmp_minima(array);
+    thrust::device_vector<float> d_tmp_maxima(array);
     
 
     
@@ -88,21 +156,25 @@ void streaming_min_max_thrust_calc(
     }*/
 
     thrust::counting_iterator<int> c_begin(0);
-    thrust::counting_iterator<int> c_end(d_vec.size() - width + 1); // inclusive end?
+    //thrust::counting_iterator<int> c_end(d_vec.size() - width + 1); // inclusive end?
+    thrust::counting_iterator<int> c_end(d_vec.size());
 
     //for all shifts
     {
         thrust::for_each(c_begin, c_end,
-            ShiftedWindowMinMaxStep(
+            ShiftedWindowMinMaxStep2(
                 d_vec.data(), 
                 width, 
+                array.size(),
+                d_tmp_minima.data(), 
+                d_tmp_maxima.data(),
                 d_minima.data(), 
                 d_maxima.data()
             ));
     }
 
-    thrust::copy(d_minima.begin(), d_minima.end(), minvalues.begin()); // is this efficient?
-    thrust::copy(d_maxima.begin(), d_maxima.end(), maxvalues.begin());
+    thrust::copy(d_minima.begin(), d_minima.end() - width + 1, minvalues.begin()); // is this efficient?
+    thrust::copy(d_maxima.begin(), d_maxima.end() - width + 1, maxvalues.begin());
     
     /*thrust::host_vector<float> h_minima(minvalues.begin(), minvalues.end());
     thrust::host_vector<float> h_maxima(maxvalues.begin(), maxvalues.end());
@@ -111,20 +183,19 @@ void streaming_min_max_thrust_calc(
     h_maxima = d_maxima;*/
 
 
-
+    for(int i=505; i < 514; i++)
+        std::cout << i << ": " << array[i] << ", ";
 
     /*for(float f : thrust::host_vector<float>(d_vec))
         std::cout << f << ", ";
-    std::cout << std::endl;
+    std::cout << std::endl << std::endl;
 
-    minvalues = d_minima;
     for(float f : minvalues )
         std::cout << f << ", ";
-    std::cout << std::endl;
+    std::cout << std::endl << std::endl;
 
-    maxvalues = d_maxima;
     for(float f : maxvalues)
         std::cout << f << ", ";
-    std::cout << std::endl;*/
+    std::cout << std::endl << std::endl;*/
 
 }
